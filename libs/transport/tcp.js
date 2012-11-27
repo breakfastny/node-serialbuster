ac40af3026d55238a7bea5f9ec67f943c1d05af3
Mattias Gunneras <mattias@breakfastny.com>
2012-11-27 17:58:40 -0500

    don't use DNS module since not needed

--- libs/transport/tcp.js
+++ libs/transport/tcp.js
@@ -12,22 +12,27 @@ module.exports = TCPTransport = function (port, spec) {
   this.host = spec && spec.host || '127.0.0.1';
   this.socket = new net.Socket();
   this.open = false;
-  dns.resolve(this.host, function(err, addresses){
-    if (err){
-      self.emit('error', err);
-      return;
-    }
-    self.socket.connect(self.port, addresses[0], function(){
-      self.open = true;
-      self.emit('open');
-    });
-    self.socket.on('close', function(){
-      self.open = false;
-      self.emit('close');
-    });
-    self.socket.on('error', function(err){
-      self.emit('error', err);
-    });
+  //dns.resolve(this.host, function(err, addresses){
+    //if (err){
+      //self.emit('error', err);
+      //return;
+    //}
+    //self.socket.connect(self.port, addresses[0], function(){
+      //self.open = true;
+      //self.emit('open');
+    //});
+  //});
+  this.socket.connect(this.port, this.host, function(){
+    self.open = true;
+    self.emit('open');
+  });
+
+  this.socket.on('close', function(){
+    self.open = false;
+    self.emit('close');
+  });
+  this.socket.on('error', function(err){
+    self.emit('error', err);
   });
 };
 
