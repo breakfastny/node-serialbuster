3bfb801529b6eddeff2fb770d3fd517d07ccc755
Mattias Gunneras <mattias@breakfastny.com>
2013-02-01 11:24:44 -0500

    updated the receive util for new transports

--- bin/receive.js
+++ bin/receive.js
@@ -1,16 +1,22 @@
-var SerialPort = require("serialport").SerialPort;
-var serialport = require("serialport");
 var sb = require('..');
 var fs = require('fs');
+var colors  = require("colors");
 var argv = require('optimist')
-    .usage('Usage: $0 -p [serialport] -b [baud] -a [address (recipient)] -v [verbose] [--stream]')
+    .usage('Usage: $0 -p [serialport] -b [baud] -a [address (recipient)] -v [verbose] [--stream] [--type str|int]')
     .boolean(['stream'])
     .default({
         'b' : 9600
-      , 'a' : sb.CONSTANTS.MASTER
+      , 'a' : sb.PROTOCOL.MASTER
+      , 'type' : 'str'
     })
     .demand(['p'])
     .argv;
+
+
+var logRed = function (txt) { console.log(txt.red) };
+var logGreen = function (txt) { console.log(txt.green) };
+var logBlue = function (txt) { console.log("  "+txt.blue) };
+var logYellow = function (txt) { console.log(txt.yellow) };
 
 if (argv.v) {
   console.log("Opening connection to:");
@@ -19,29 +25,36 @@ if (argv.v) {
   console.log("");
 }
 
-var serial = new sb.SerialBuster(argv.p, {
-    'baudrate' : parseInt(argv.b, 10)
-  , 'parser' : sb.parser(0, {debug:true})  
-  , 'buffersize' : 1024
+var transport = new SerialTransport(argv.p, {
+    'baudrate' : argv.b
 });
 
-serial.on('packet', function (packet) {
-  if (argv.stream) {
-    process.stdout.write(packet.payload);
+var buster = new sb.SerialBuster(transport, {
+    'address' : parseInt(argv.a, 10)
+});
+
+transport.on('open', function (){
+  logGreen('Transport opened');
+});
+
+transport.on('error', function(err) {
+  console.log(err);
+});
+
+transport.on('close', function() {
+  logRed('Transport closed');
+});
+
+buster.on('packet', function (packet) {
+  if(argv.type == 'int'){
+    var out = packet.payload.readInt16LE(0);
   }else{
-    console.log('Recived packet to: '+packet.recipient+' from: '+packet.sender+' payload: "'+packet.payload+'" length: '+packet.toData().length);
-    if (argv.v) {
-      for (var i=0; i < packet.payload.length; i++) {
-        console.log('after unescape: ascii: '+String.fromCharCode(packet.payload[i])+' dec: '+packet.payload[i]);
-      };
-    }
+    var out = packet.payload.toString();
+  }
+  logYellow('Recived packet to: '+packet.recipient+' from: '+packet.sender+' payload: "'+out+'" length: '+packet.toData().length);
+  if (argv.v) {
+    for (var i=0; i < packet.payload.length; i++) {
+      logBlue('after unescape: ascii: '+String.fromCharCode(packet.payload[i])+' dec: '+packet.payload[i]);
+    };
   }
 });
-
-if (argv.v) {
-  serial.on('data', function (buffer) {
-    for (var i=0; i < buffer.length; i++) {
-      console.log('before unescape: ascii: '+String.fromCharCode(buffer[i])+' dec: '+buffer[i]);
-    };
-  });
-}
